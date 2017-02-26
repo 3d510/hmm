ac71bbc0bf0809cf5d252b77458040fe808b2415
caluuvi <claudia.villanuevac@pucp.pe>
2017-02-26 15:44:08 +0100

    first advance

--- src/model/DummyLocalizer.java
+++ src/model/DummyLocalizer.java
@@ -1,16 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import control.EstimatorInterface;
 
 public class DummyLocalizer implements EstimatorInterface {
 		
-	private int rows, cols, head;
+	private int rows, cols, head, posX, posY;
+	Random rand = new Random();
+	int[] xDir = {0,-1,0,1}; // LEFT,UP,RIGHT,DOWN
+	int[] yDir = {-1,0,1,0};
 
 	public DummyLocalizer( int rows, int cols, int head) {
 		this.rows = rows;
 		this.cols = cols;
-		this.head = head;
-		
+		this.head = head;		
+		this.posX = rand.nextInt(rows);
+		this.posY = rand.nextInt(cols);
 	}	
 	
 	public int getNumRows() {
@@ -35,16 +43,41 @@ public class DummyLocalizer implements EstimatorInterface {
 
 
 	public int[] getCurrentTruePosition() {
-		
 		int[] ret = new int[2];
-		ret[0] = rows/2;
-		ret[1] = cols/2;
+		
+		if(isFacingWall()){
+			int dirIndex = randDirection(); 
+			ret[0] = posX + xDir[dirIndex];
+			ret[1] = posY + yDir[dirIndex];
+		} else {
+			double prob = rand.nextDouble();
+			if (prob<=0.3) {
+				int dirIndex = randDirection(); 
+				ret[0] = posX + xDir[dirIndex];
+				ret[1] = posY + yDir[dirIndex];
+			} else {
+				ret[0] = posX + xDir[head];
+				ret[1] = posY + yDir[head];
+			}
+		}
+		posX = ret[0];
+		posY = ret[1];
 		return ret;
-
 	}
 
 	public int[] getCurrentReading() {
-		int[] ret = null;
+		int[] ret = new int[2];
+		double prob = rand.nextDouble();
+		
+		if(prob <= 0.1){
+			ret[0] = posX;
+			ret[1] = posY;
+		}else if(prob <= 0.1 + 8 * 0.5){
+			ret = getRandomLoc1();
+		}else if(prob <= 0.1 + 8 * 0.5 + 16 * 0.025)
+			ret = getRandomLoc2();
+		else
+			ret = null;
 		return ret;
 	}
 
@@ -58,5 +91,89 @@ public class DummyLocalizer implements EstimatorInterface {
 		System.out.println("Nothing is happening, no model to go for...");
 	}
 	
+	public int[] getRandomLoc1(){
+		int [] ret = {-1,-1};
+		List<ArrayList<Integer> > firstField = new ArrayList<ArrayList<Integer>>();
+		
+		int[] xDiff = {-1,-1,-1,0,0,1,1,1};
+		int[] yDiff = {-1,0,1,-1,1,-1,0,1};
+		
+		for (int i=0; i<8; i++) {
+			ArrayList<Integer> adjPos = new ArrayList<Integer>();
+			adjPos.add(posX+xDiff[i]);
+			adjPos.add(posY+yDiff[i]);
+			firstField.add(adjPos);
+		}
+		
+		int randIndex = rand.nextInt(8);
+		
+		ArrayList<Integer> selected = firstField.get(randIndex);
+		int xSelected = selected.get(0);
+		int ySelected = selected.get(1);
+		
+		if(isOutside(xSelected,ySelected))
+			return null;
+		
+		ret[0] = xSelected;
+		ret[1] = ySelected;
+		
+		return ret;
+	}
+	
+	public int[] getRandomLoc2(){
+		int [] ret = {-1,-1};
+		List<ArrayList<Integer> > secondField = new ArrayList<ArrayList<Integer>>();
+		
+		int[] xDiff = {-2,-2,-2,-2,-2,-1,-1,0,0,1,1,2,2,2,2,2};
+		int[] yDiff = {-2,-1,0,1,2,-2,2,-2,2,-2,2,-2,-1,0,1,2};
+		
+		for (int i=0; i<16; i++) {
+			ArrayList<Integer> adjPos = new ArrayList<Integer>();
+			adjPos.add(posX+xDiff[i]);
+			adjPos.add(posY+yDiff[i]);
+			secondField.add(adjPos);
+		}
+		
+		int randIndex = rand.nextInt(16);
+		
+		ArrayList<Integer> selected = secondField.get(randIndex);
+		int xSelected = selected.get(0);
+		int ySelected = selected.get(1);
+		
+		if(isOutside(xSelected,ySelected))
+			return null;
+		
+		ret[0] = xSelected;
+		ret[1] = ySelected;
+		
+		return ret;
+	}
+	
+	public boolean isFacingWall() {
+		int newX = posX + xDir[head];
+		int newY = posY + yDir[head];
+		return isOutside(newX, newY);
+	}
+	
+//	public boolean isInCorner() {
+//		if (posX!=0 || posX!=rows-1 || posY!=0 || posY!=cols-1)
+//			return false;
+//		if (posX==0) 
+//			return posY==0 || posY==cols-1;
+//		return posX == 0 || posX==rows-1;
+//	}
+	
+	public boolean isOutside(int xCoord, int yCoord) {
+		return xCoord < 0 || xCoord >= rows || yCoord < 0 || yCoord >= cols;
+	}
+	
+	public int randDirection(){
+		ArrayList<Integer> possibleDir = new ArrayList<Integer>();
+		for (int i=0; i<4; i++)
+			if (i!=head && !isOutside(posX+xDir[i],posY+yDir[i]))
+				possibleDir.add(i);
+		int randIndex = rand.nextInt(possibleDir.size());
+		return possibleDir.get(randIndex);
+	}
 	
 }
